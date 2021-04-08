4bd8a3b0d62bf390ef05b7fe40589654788e83fa
noah the goodra <peterpan0413@live.com>
2021-04-08 16:15:28 -0500

    remove the lexer for now

--- Program.cs
+++ Program.cs
@@ -7,18 +7,6 @@ namespace SkyrimPapCompilerExtensionFramework
     {
 	    public static void Main(string[] args)
         {
-            var input = "alert( 'Hello, world!' );";
-
-            var chars = new Antlr.Runtime.ANTLRStringStream(input);
-            var lexer = new JavaScriptLexer(chars);
-            //lexer.strictMode = false; // do not use js strictMode
-
-            var tokens = new Antlr.Runtime.CommonTokenStream(lexer);
-            var parser = new JavaScriptParser(tokens);
-            var tree = parser.program();
-
-            Console.WriteLine(tree.Tree.ToStringTree());
-            Console.WriteLine(parser.kObjType);
             //Console.Write(myscript);
             //testme2.OutputAST("testme.dot", myscript.kAST);
             PapyrusCompiler.Program.Main(args);
